8befcebeadaf2becfc43723e9074d901ed9e3065
Jhon Bellido <jhonbellidovargas@gmail.com>
2022-11-21 10:03:12 -0500

    feat: index main

--- Tarea15/js/funciones.js
+++ Tarea15/js/funciones.js
@@ -1,9 +1,3 @@
-// Ejercicio 1: Digite el importe de una compra, en caso de que el importe sea 150 o más, se le
-//descontará el 12%. Mostrar el descuento otorgado y el importe de compra final
-// Ejercicio 2: Ingrese el nombre de un alumno y las notas de su examen parcial, examen final y el
-//  promedio de prácticas; muestre el nombre del alumno y su promedio final solo si el alumno está aprobado.
-//  Tenga en cuenta que para el cálculo del promedio la nota del examen final tiene peso doble.
-
 // Ejercicio 1
 const res = document.getElementById("resultado");
 let importe = prompt("Importe de compra:");
